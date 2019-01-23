1f14129c02ef34c5aafe00451c6c9161ad6560e0
David L Barrero <>
2019-01-23 09:48:10 +0100

    Cambios

--- src/app/register/register.page.ts
+++ src/app/register/register.page.ts
@@ -10,22 +10,26 @@ import { Router } from '@angular/router';
 })
 export class RegisterPage {
 
-  username: string = '';
-  password: string = '';
-  cpassword: string = '';
+  email: string ;
+  password: string ;
+  cpassword: string;
 
   constructor(public afr: AngularFireAuth, public rout: Router) { }
 
+  // Funcion registro
+
   async register() {
 
-    const { username, password, cpassword } = this;
+    const { email, password, cpassword } = this;
+
+    // Comprobacion contraseña
 
     if (password !== cpassword) {
       return console.error('las contraseñas no son iguales');
     }
-
+    // Registro con email y contraseña, cambio el username por email
     try {
-      const res = this.afr.auth.createUserWithEmailAndPassword(username, password);
+      const res = this.afr.auth.createUserWithEmailAndPassword(email, password);
       console.log(res);
       this.rout.navigateByUrl('/login');
     } catch (error) {
@@ -34,7 +38,7 @@ export class RegisterPage {
 
 
   }
-
+  // Registro con google
   async registerGmail() {
 
     try {
