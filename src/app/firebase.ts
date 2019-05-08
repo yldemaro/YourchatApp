0b5e9713f1ad50cac6d89e0760c42e1aa79dd969
yldemaro jimenez <ylde198989@gmail.com>
2019-05-08 09:42:42 -0400

    Update firebase.ts

--- src/app/firebase.ts
+++ src/app/firebase.ts
@@ -1,10 +1,10 @@
 const Config = {
-    apiKey: "AIzaSyCGK-GrAXnf1nB0QPjBIhmaqn3tCJW6rOU",
-    authDomain: "yourchat-f95bb.firebaseapp.com",
-    databaseURL: "https://yourchat-f95bb.firebaseio.com",
-    projectId: "yourchat-f95bb",
-    storageBucket: "yourchat-f95bb.appspot.com",
-    messagingSenderId: "666730600912"
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: ""
 };
 
 export default Config;
